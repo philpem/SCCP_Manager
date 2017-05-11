0c3f91ac501b098ee22cb46b34abb9836c119bb2
qoole <qoole.github@lillimoth.com>
2017-05-11 21:04:08 +0100

    Update install.php
    Only FreePBX 14+ creates databases from module.xml so do create
    sccpdevmodel database if we are on FreePBX <14.

--- install.php
+++ install.php
@@ -2,38 +2,47 @@
 
 
 #sccpdevmodel
-$sql = 'SHOW COLUMNS FROM sccpdevmodel WHERE FIELD = "id"';
-$sth = FreePBX::Database()->prepare($sql);
-$sth->execute();
-$array = $sth->fetchAll(PDO::FETCH_ASSOC);
-if (empty($array)) 
+if (!tableExists("sccpdevmodel") && version_compare(get_framework_version(),"14.0",">=")) 
 {
-		die_freepbx("Can not find sccpdevmodel table! This table should have been created by module.xml. Please check your installation\n");
+	#FreePBX 14+ will create the database from the module.xml
+	die_freepbx("Can not find sccpdevmodel table! This table should have been created by module.xml. Please check your installation\n" . get_framework_version());
 }
-else
+else if (!tableExists("sccpdevmodel") && version_compare(get_framework_version(),"14.0","<"))
 {
-	$sql = 'SELECT * FROM sccpdevmodel';
+	#FreePBX 13 and lower we have to create the DB
+	$sql = "CREATE TABLE IF NOT EXISTS `sccpdevmodel` (
+    `model` varchar(20) NOT NULL DEFAULT '',
+    `vendor` varchar(40) DEFAULT '',
+    `dns` int(2) DEFAULT '1',
+    `buttons` int(2) DEFAULT '0',
+    `loadimage` varchar(40) DEFAULT '',
+	`loadinformationid` varchar(30) DEFAULT '',
+    PRIMARY KEY (`model`),
+    KEY `model` (`model`)
+    ) ENGINE=MyISAM DEFAULT CHARSET=latin1";
 	$sth = FreePBX::Database()->prepare($sql);
 	$sth->execute();
-	$array = $sth->fetchAll(PDO::FETCH_ASSOC);
-	if (empty($array)) 
+}
+
+
+if (tableEmpty("sccpdevmodel")) 
+{
+	$sql = "REPLACE INTO `sccpdevmodel` VALUES ('7925','CISCO',1,1,'',''),('7902','CISCO',1,1,'CP7902080002SCCP060817A','loadInformation30008'),('7905','CISCO',1,1,'CP7905080003SCCP070409A','loadInformation20000'),('7906','CISCO',1,1,'SCCP11.8-3-1S','loadInformation369'),('7910','CISCO',1,1,'P00405000700','loadInformation6'),('7911','CISCO',1,1,'SCCP11.8-3-1S','loadInformation307'),('7912','CISCO',1,1,'CP7912080003SCCP070409A','loadInformation30007'),('7914','CISCO',0,14,'S00105000300','loadInformation124'),('7920','CISCO',1,1,'cmterm_7920.4.0-03-02','loadInformation30002'),('7921','CISCO',1,1,'CP7921G-1.0.3','loadInformation365'),('7931','CISCO',1,1,'SCCP31.8-3-1S','loadInformation348'),('7936','CISCO',1,1,'cmterm_7936.3-3-13-0','loadInformation30019'),('7937','CISCO',1,1,'','loadInformation431'),('7940','CISCO',1,2,'P00308000500','loadInformation8'),('Digital Access+','CISCO',1,1,'D00303010033','loadInformation42'),('7941','CISCO',1,2,'P00308000500','loadInformation115'),('7941G-GE','CISCO',1,2,'P00308000500','loadInformation309'),('7942','CISCO',1,2,'P00308000500','loadInformation434'),('Digital Access','CISCO',1,1,'D001M022','loadInformation40'),('7945','CISCO',1,2,'P00308000500','loadInformation435'),('7960','CISCO',3,6,'P00308000500','loadInformation7'),('7961','CISCO',3,6,'P00308000500','loadInformation30018'),('7961G-GE','CISCO',3,6,'P00308000500','loadInformation308'),('7962','CISCO',3,6,'P00308000500','loadInformation404'),('7965','CISCO',3,6,'P00308000500','loadInformation436'),('7970','CISCO',3,8,'SCCP70.8-3-1S','loadInformation30006'),('7971','CISCO',3,8,'SCCP70.8-3-1S','loadInformation119'),('7975','CISCO',3,8,'SCCP70.8-3-1S','loadInformation437'),('7985','CISCO',3,8,'cmterm_7985.4-1-4-0','loadInformation302'),('ATA 186','CISCO',1,1,'ATA030203SCCP051201A','loadInformation12'),('IP Communicator','CISCO',1,1,'','loadInformation30016'),('12 SP','CISCO',1,1,'','loadInformation3'),('12 SP+','CISCO',1,1,'','loadInformation2'),('30 SP+','CISCO',1,1,'','loadInformation1'),('30 VIP','CISCO',1,1,'','loadInformation5'),('7914,7914','CISCO',0,28,'S00105000300','loadInformation124'),('7915','CISCO',0,14,'',''),('7916','CISCO',0,14,'',''),('7915,7915','CISCO',0,28,'',''),('7916,7916','CISCO',0,28,'',''),('CN622','MOTOROLA',1,1,'','loadInformation335'),('ICC','NOKIA',1,1,'',''),('E-Series','NOKIA',1,1,'',''),('3911','CISCO',1,1,'','loadInformation446'),('3951','CISCO',1,1,'','loadInformation412');";
+	$sth = FreePBX::Database()->prepare($sql);
+	try
 	{
-		$sql = "REPLACE INTO `sccpdevmodel` VALUES ('7925','CISCO',1,1,'',''),('7902','CISCO',1,1,'CP7902080002SCCP060817A','loadInformation30008'),('7905','CISCO',1,1,'CP7905080003SCCP070409A','loadInformation20000'),('7906','CISCO',1,1,'SCCP11.8-3-1S','loadInformation369'),('7910','CISCO',1,1,'P00405000700','loadInformation6'),('7911','CISCO',1,1,'SCCP11.8-3-1S','loadInformation307'),('7912','CISCO',1,1,'CP7912080003SCCP070409A','loadInformation30007'),('7914','CISCO',0,14,'S00105000300','loadInformation124'),('7920','CISCO',1,1,'cmterm_7920.4.0-03-02','loadInformation30002'),('7921','CISCO',1,1,'CP7921G-1.0.3','loadInformation365'),('7931','CISCO',1,1,'SCCP31.8-3-1S','loadInformation348'),('7936','CISCO',1,1,'cmterm_7936.3-3-13-0','loadInformation30019'),('7937','CISCO',1,1,'','loadInformation431'),('7940','CISCO',1,2,'P00308000500','loadInformation8'),('Digital Access+','CISCO',1,1,'D00303010033','loadInformation42'),('7941','CISCO',1,2,'P00308000500','loadInformation115'),('7941G-GE','CISCO',1,2,'P00308000500','loadInformation309'),('7942','CISCO',1,2,'P00308000500','loadInformation434'),('Digital Access','CISCO',1,1,'D001M022','loadInformation40'),('7945','CISCO',1,2,'P00308000500','loadInformation435'),('7960','CISCO',3,6,'P00308000500','loadInformation7'),('7961','CISCO',3,6,'P00308000500','loadInformation30018'),('7961G-GE','CISCO',3,6,'P00308000500','loadInformation308'),('7962','CISCO',3,6,'P00308000500','loadInformation404'),('7965','CISCO',3,6,'P00308000500','loadInformation436'),('7970','CISCO',3,8,'SCCP70.8-3-1S','loadInformation30006'),('7971','CISCO',3,8,'SCCP70.8-3-1S','loadInformation119'),('7975','CISCO',3,8,'SCCP70.8-3-1S','loadInformation437'),('7985','CISCO',3,8,'cmterm_7985.4-1-4-0','loadInformation302'),('ATA 186','CISCO',1,1,'ATA030203SCCP051201A','loadInformation12'),('IP Communicator','CISCO',1,1,'','loadInformation30016'),('12 SP','CISCO',1,1,'','loadInformation3'),('12 SP+','CISCO',1,1,'','loadInformation2'),('30 SP+','CISCO',1,1,'','loadInformation1'),('30 VIP','CISCO',1,1,'','loadInformation5'),('7914,7914','CISCO',0,28,'S00105000300','loadInformation124'),('7915','CISCO',0,14,'',''),('7916','CISCO',0,14,'',''),('7915,7915','CISCO',0,28,'',''),('7916,7916','CISCO',0,28,'',''),('CN622','MOTOROLA',1,1,'','loadInformation335'),('ICC','NOKIA',1,1,'',''),('E-Series','NOKIA',1,1,'',''),('3911','CISCO',1,1,'','loadInformation446'),('3951','CISCO',1,1,'','loadInformation412');";
-		$check = $db->query($sql);
-		if(DB::IsError($check)) 
-		{
-			die_freepbx("Can not REPLACE defaults into sccpdevmodel table\n");
-		}
+		$sth->execute();
+	}
+	catch (PDOException $exception)
+	{
+		die_freepbx("Can not REPLACE defaults into sccpdevmodel table\n" . $exception->getMessage() . "\n");
 	}
 }
 
 
+
 #sccpline
-$sql = 'SHOW COLUMNS FROM sccpline WHERE FIELD = "id"';
-$sth = FreePBX::Database()->prepare($sql);
-$sth->execute();
-$array = $sth->fetchAll(PDO::FETCH_ASSOC);
-if (empty($array)) 
+if (!tableExists("sccpline")) 
 {
 	die_freepbx("Can not find sccpline table, you must install chan_sccp and configure realtime databases (sccpline and sccpdevice) before installing sccp-manager!\n");
 }
@@ -59,11 +68,7 @@ else
 
 
 #sccpdevice
-$sql = 'SHOW COLUMNS FROM sccpdevice WHERE FIELD = "id"';
-$sth = FreePBX::Database()->prepare($sql);
-$sth->execute();
-$array = $sth->fetchAll(PDO::FETCH_ASSOC);
-if (empty($array)) 
+if (!tableExists("sccpdevice")) 
 {
     
     die_freepbx("Can not find sccpdevice table, you must install chan_sccp and configure realtime databases (sccpline, sccpdevice, buttonconfig) before installing sccp-manager!\n");
@@ -97,12 +102,54 @@ else
 }
 
 #buttonconfig
-$sql = 'SHOW COLUMNS FROM buttonconfig WHERE FIELD = "device"';
-$sth = FreePBX::Database()->prepare($sql);
-$sth->execute();
-$array = $sth->fetchAll(PDO::FETCH_ASSOC);
-if (empty($array)) 
+if (!tableExists("buttonconfig")) 
 {
     die_freepbx("Can not find buttonconfig table, you must install chan_sccp and configure realtime databases (sccpline, sccpdevice and buttonconfig) before installing sccp-manager!\n");
 }
 
+
+function tableExists($tableName) 
+{
+    $mrSql = "SHOW TABLES LIKE :table_name";
+    $mrStmt = FreePBX::Database()->prepare($mrSql);
+    //protect from injection attacks
+    $mrStmt->bindParam(":table_name", $tableName, PDO::PARAM_STR);
+
+    $sqlResult = $mrStmt->execute();
+    if ($sqlResult) {
+        $row = $mrStmt->fetch(PDO::FETCH_NUM);
+        if ($row[0]) {
+            //table was found
+            return true;
+        } else {
+            //table was not found
+            return false;
+        }
+    } else {
+        //some PDO error occurred
+        echo("Could not check if table exists, Error: ".var_export($pdo->errorInfo(), true));
+        return false;
+    }
+}
+
+function tableEmpty($tableName) 
+{
+    $mrSql = "SELECT * FROM " . $tableName;
+    $mrStmt = FreePBX::Database()->prepare($mrSql);
+
+    $sqlResult = $mrStmt->execute();
+    if ($sqlResult) {
+        $row = $mrStmt->fetch(PDO::FETCH_NUM);
+        if ($row[0]) {
+            //a row was found
+            return false;
+        } else {
+            //a row was not found
+            return true;
+        }
+    } else {
+        //some PDO error occurred
+        echo("Could not check if table was empty, Error: ".var_export($pdo->errorInfo(), true));
+        return false;
+    }
+}
